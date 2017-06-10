4aa9e5ea50f5ebe8a4d9b068a612e2bc3ffd0e89
Sanjay Mahto <mahto.sanjay41@gmail.com>
2017-06-10 11:07:37 +0530

    Add files via upload

--- Angular/angular/app.js
+++ Angular/angular/app.js
@@ -1,6 +1,8 @@
+// first we have to declare the module. note that [] is where we will declare the dependecies later. Right now we are leaving it blank
 var myApp = angular.module('myGameApp', ['ngRoute']);
 
-//Beginning of Team View Module for season 2015-16
+
+// this is without $scope
 myApp.controller('mainControllerTeamView2015', ['$http', '$scope', function ($http, $scope) {
 
     //create a context
@@ -42,14 +44,13 @@ myApp.controller('mainControllerTeamView2015', ['$http', '$scope', function ($ht
 
     this.loadAllMatches();
     $scope.text = '';
-
+    $scope.error = "";
+    $scope.codename = "";
 
     $scope.submit = function () {
         console.log($scope.text);
         if ($scope.text) {
             $scope.count = 0;
-            $scope.error = "";
-            $scope.codename = "";
             $scope.matchwon = '';
             $scope.matchlost = '';
             $scope.matchdraw = '';
@@ -65,43 +66,52 @@ myApp.controller('mainControllerTeamView2015', ['$http', '$scope', function ($ht
                         ++($scope.count);
                         if ((main.matches2015.rounds[i].matches[j].score1) > (main.matches2015.rounds[i].matches[j].score2)) {
                             ++($scope.matchwon);
-                        } else {
-                            if ((main.matches2015.rounds[i].matches[j].score1) == (main.matches2015.rounds[i].matches[j].score2)) {
-                                ++($scope.matchdraw);
-                            } else {
-                                ++($scope.matchlost);
-                            }
-
+                        }
+                       else{
+                                 if ((main.matches2015.rounds[i].matches[j].score1) == (main.matches2015.rounds[i].matches[j].score2)) {
+                                   ++($scope.matchdraw);
+                               }
+                                 
+                                       else
+                                {
+                                    ++($scope.matchlost);
+                                }
+                                
                         }
                         break;
 
                     } else {
                         if (($scope.text.toUpperCase()) == (main.matches2015.rounds[i].matches[j].team2.name.toUpperCase())) {
-                            // console.log("team 2");
+                           // console.log("team 2");
                             $scope.error = "";
                             ++($scope.count);
                             $scope.codename = (main.matches2015.rounds[i].matches[j].team2.code);
-                            if ((main.matches2015.rounds[i].matches[j].score1) < (main.matches2015.rounds[i].matches[j].score2)) {
-                                ++($scope.matchwon);
-                            } else {
-                                if ((main.matches2015.rounds[i].matches[j].score1) == (main.matches2015.rounds[i].matches[j].score2)) {
-                                    ++($scope.matchdraw);
-                                } else {
+                               if ((main.matches2015.rounds[i].matches[j].score1) < (main.matches2015.rounds[i].matches[j].score2)) {
+                                   ++($scope.matchwon);
+                               }
+                               else{
+                                 if ((main.matches2015.rounds[i].matches[j].score1) == (main.matches2015.rounds[i].matches[j].score2)) {
+                                   ++($scope.matchdraw);
+                               }
+                                 
+                                       else
+                                {
                                     ++($scope.matchlost);
                                 }
-
-                            }
+                                
+                        }
                             break;
                         }
-
+                        
                     }
                 }
             }
-            if ($scope.matchwon == 0) {
+            if ($scope.matchwon == 0 && $scope.count != 0) {
                 $scope.matchwon = "No match won by this Team.";
             }
             if ($scope.count == 0) {
                 $scope.error = "Sorry! Either the Team hasn't played in this Season or Wrong team name entered...";
+                $scope.codename="";
                 console.log("Sorry Team not Present!");
 
             }
@@ -112,10 +122,8 @@ myApp.controller('mainControllerTeamView2015', ['$http', '$scope', function ($ht
     }
 
  }]); // end controller*/
-//End of team view module for season 2015-16
 
 
-//Begining of team view module for season 2016-17
 myApp.controller('mainControllerTeamView2016', ['$http', '$scope', function ($http, $scope) {
 
     //create a context
@@ -164,8 +172,6 @@ myApp.controller('mainControllerTeamView2016', ['$http', '$scope', function ($ht
         console.log($scope.text);
         if ($scope.text) {
             $scope.count = 0;
-            $scope.error = "";
-            $scope.codename = "";
             $scope.matchwon = '';
             $scope.matchlost = '';
             $scope.matchdraw = '';
@@ -181,13 +187,16 @@ myApp.controller('mainControllerTeamView2016', ['$http', '$scope', function ($ht
                         ++($scope.count);
                         if ((main.matches2016.rounds[i].matches[j].score1) > (main.matches2016.rounds[i].matches[j].score2)) {
                             ++($scope.matchwon);
-                        } else {
-                            if ((main.matches2016.rounds[i].matches[j].score1) == (main.matches2016.rounds[i].matches[j].score2)) {
-                                ++($scope.matchdraw);
-                            } else {
+                        }
+                       else{
+                        if ((main.matches2016.rounds[i].matches[j].score1) == (main.matches2016.rounds[i].matches[j].score2)) {
+                            ++($scope.matchdraw);
+                        }
+                     else
+                            {
                                 ++($scope.matchlost);
                             }
-                        }
+                    }
                         break;
 
                     } else {
@@ -196,25 +205,32 @@ myApp.controller('mainControllerTeamView2016', ['$http', '$scope', function ($ht
                             $scope.error = "";
                             ++($scope.count);
                             $scope.codename = (main.matches2016.rounds[i].matches[j].team2.code);
-                            if ((main.matches2016.rounds[i].matches[j].score1) < (main.matches2016.rounds[i].matches[j].score2)) {
-                                ++($scope.matchwon);
-                            } else {
-                                if ((main.matches2016.rounds[i].matches[j].score1) == (main.matches2016.rounds[i].matches[j].score2)) {
-                                    ++($scope.matchdraw);
-                                } else {
+                              if ((main.matches2016.rounds[i].matches[j].score1) < (main.matches2016.rounds[i].matches[j].score2)) {
+                                   ++($scope.matchwon);
+                               }
+        
+                             else{
+                                 if ((main.matches2016.rounds[i].matches[j].score1) == (main.matches2016.rounds[i].matches[j].score2)) {
+                                   ++($scope.matchdraw);
+                               }
+                                 
+                                       else
+                                {
                                     ++($scope.matchlost);
                                 }
-                            }
+                                
+                        }
                             break;
                         }
                     }
                 }
             }
-            if ($scope.matchwon == 0) {
+            if ($scope.matchwon == 0 && $scope.count != 0) {
                 $scope.matchwon = "No match won by this Team.";
             }
             if ($scope.count == 0) {
                 $scope.error = "Sorry! Either the Team hasn't played in this Season or Wrong team name entered...";
+                $scope.codename="";
                 console.log("Sorry Team not Present!");
 
             }
@@ -225,11 +241,15 @@ myApp.controller('mainControllerTeamView2016', ['$http', '$scope', function ($ht
     }
 
  }]); // end controller*/
-//End of team view module for season 2016-17
 
 
-//Module for single team view and all team view for 2015-16
-myApp.controller('mainController2015', ['$http', function ($http) {
+
+
+//End of the single team view...
+
+
+// this is without $scope
+myApp.controller('mainController2015', ['$http','$scope', function ($http,$scope) {
 
     //create a context
     var main = this;
@@ -269,6 +289,76 @@ myApp.controller('mainController2015', ['$http', function ($http) {
     } // end load all matches
 
     this.loadAllMatches();
+   $scope.myFunction = function(matchDate,name1,name2) {
+
+        console.log("hello sanjay!!");
+        console.log(matchDate);
+        console.log(name1);
+        console.log(name2);
+
+
+        $http({
+            method: 'GET',
+            url: main.baseUrl
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response);
+            main.matches2015 = response.data;
+            for (var i = 0; i < main.matches2015.rounds.length; i++) {
+                var roundname = main.matches2015.rounds[i].name;
+                for (var j = 0; j < main.matches2015.rounds[i].matches.length; j++) {
+
+                    if((main.matches2015.rounds[i].matches[j].date == matchDate) && (main.matches2015.rounds[i].matches[j].team1.name == name1 && main.matches2015.rounds[i].matches[j].team2.name == name2))
+                    {
+                    	
+                    	var winningTeam = "";
+                    	$scope.matchDay = roundname;
+                    	$scope.date = matchDate;
+                    	$scope.teamname1 = main.matches2015.rounds[i].matches[j].team1.name;
+                    	$scope.teamname2 = main.matches2015.rounds[i].matches[j].team2.name;
+                    	$scope.score1 = main.matches2015.rounds[i].matches[j].score1;
+                    	$scope.score2 = main.matches2015.rounds[i].matches[j].score2;
+                    	if(main.matches2015.rounds[i].matches[j].score1>main.matches2015.rounds[i].matches[j].score2)
+                    	{
+                    		 winningTeam = main.matches2015.rounds[i].matches[j].team1.name;
+                    		 losingTeam = main.matches2015.rounds[i].matches[j].team2.name;
+                    	}
+                    	else
+                    	{
+                    		winningTeam = main.matches2015.rounds[i].matches[j].team2.name;
+                    		losingTeam = main.matches2015.rounds[i].matches[j].team1.name;
+                    	}
+                    	$scope.win = winningTeam;
+                    	$scope.lost = losingTeam;
+                    	if(main.matches2015.rounds[i].matches[j].score1 == main.matches2015.rounds[i].matches[j].score2)
+                    	{
+                    		$scope.win ="";
+                    	$scope.lost = "";
+                    		$scope.draw = "match draw...";
+                    	}
+                    	else
+                    	{
+                    		$scope.draw = " ";
+                    	}
+                    	
+                    }
+                    else
+                    {
+                    	continue;
+                    }
+                   
+                }
+            }
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            alert("some error occurred. Check the console.");
+            console.log(response);
+
+        });
+
+    }
 
 
 
@@ -276,8 +366,8 @@ myApp.controller('mainController2015', ['$http', function ($http) {
 }]); // end controller
 
 
-//Module for single team view and all team view for 2016-17
-myApp.controller('mainController2016', ['$http', function ($http) {
+//main Controller for 2016 matches
+myApp.controller('mainController2016', ['$http','$scope', function ($http,$scope) {
 
     //create a context
     var main = this;
@@ -310,4 +400,73 @@ myApp.controller('mainController2016', ['$http', function ($http) {
 
     this.loadAllMatches();
 
+$scope.myFunction = function(matchDate,name1,name2) {
+
+        console.log(matchDate);
+        console.log(name1);
+        console.log(name2);
+        $http({
+            method: 'GET',
+            url: main.baseUrl
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response);
+            main.matches2016 = response.data;
+            for (var i = 0; i < main.matches2016.rounds.length; i++) {
+                var roundname = main.matches2016.rounds[i].name;
+                for (var j = 0; j < main.matches2016.rounds[i].matches.length; j++) {
+
+                    if((main.matches2016.rounds[i].matches[j].date == matchDate) && (main.matches2016.rounds[i].matches[j].team1.name == name1 && main.matches2016.rounds[i].matches[j].team2.name == name2))
+                    {
+                    	
+                    	var winningTeam = "";
+                    	$scope.matchDay = roundname;
+                    	$scope.date = matchDate;
+                    	$scope.teamname1 = main.matches2016.rounds[i].matches[j].team1.name;
+                    	$scope.teamname2 = main.matches2016.rounds[i].matches[j].team2.name;
+                    	$scope.score1 = main.matches2016.rounds[i].matches[j].score1;
+                    	$scope.score2 = main.matches2016.rounds[i].matches[j].score2;
+                    	if(main.matches2016.rounds[i].matches[j].score1>main.matches2016.rounds[i].matches[j].score2)
+                    	{
+                    		 winningTeam = main.matches2016.rounds[i].matches[j].team1.name;
+                    		 losingTeam = main.matches2016.rounds[i].matches[j].team2.name;
+                    	}
+                    	else
+                    	{
+                    		winningTeam = main.matches2016.rounds[i].matches[j].team2.name;
+                    		losingTeam = main.matches2016.rounds[i].matches[j].team1.name;
+                    	}
+                    	$scope.win = winningTeam;
+                    	$scope.lost = losingTeam;
+                    	if(main.matches2016.rounds[i].matches[j].score1 == main.matches2016.rounds[i].matches[j].score2)
+                    	{
+                    		$scope.win ="";
+                    	$scope.lost = "";
+                    		$scope.draw = "match draw...";
+                    	}
+                    	else
+                    	{
+                    		$scope.draw = " ";
+                    	}
+                    	
+                    }
+                    else
+                    {
+                    	continue;
+                    }
+                   
+                }
+            }
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            alert("some error occurred. Check the console.");
+            console.log(response);
+
+        });
+
+    }
+
+  
 }]); // end controller
